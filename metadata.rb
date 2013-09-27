e847d5e92685e8f94fc33e6cd2b27aeb03e2be93
Phil Cohen <github@phlippers.net>
2013-09-26 22:27:10 -0700

    bump version to 1.1.0

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,10 @@ maintainer       "Phil Cohen"
 maintainer_email "github@phlippers.net"
 license          "MIT"
 description      "Installs zip and unzip"
-version          "1.0.0"
+version          "1.1.0"
 
 recipe "zip", "Installs zip and unzip"
 
 supports "ubuntu"
+supports "redhat"
+supports "centos"
