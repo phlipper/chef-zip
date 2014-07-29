c1e0e4c5d3e7308e409821bd488fab5193524a55
Ladislav Gazo <gazo@seges.sk>
2014-07-29 11:12:28 +0200

    implemented zip extraction LWRP resource

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Phil Cohen"
 maintainer_email "github@phlippers.net"
 license          "MIT"
 description      "Installs zip and unzip"
-version          "1.1.0"
+version          "1.2.0"
 
 recipe "zip", "Installs zip and unzip"
 
---
+++ providers/default.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: zip
+# Provider:: default
+#
+
+# Support whyrun
+def whyrun_supported?
+  true
+end
+
+action :extract do
+  description = "extract #{new_resource.name} ( #{new_resource.source} )"
+  converge_by(description) do
+    if new_resource.source == nil
+      source = new_resource.name
+    else
+      source = new_resource.source
+    end
+
+    if new_resource.path == nil
+      extract_path = "."
+    else
+      extract_path = new_resource.path
+    end
+
+    source_dir = ::File.dirname(source)
+    log "extracting ZIP #{source} to #{extract_path} in #{source_dir}"
+
+    bash "extract_#{source}" do
+      cwd source_dir
+      code <<-EOH
+        unzip #{source} -d #{extract_path}
+        chown -R #{new_resource.owner}:#{new_resource.group} #{extract_path}
+        EOH
+      #not_if { ::File.exists?(extract_path) }
+    end
+
+
+  end
+end
+
